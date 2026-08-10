92990a2ec273a04163456b3bbf6606bc1a4a1794
Harsh Mahajan <harsh.mahajan@thinkitive.com>
2026-08-10 12:09:06 +0530

    patient controller added

---
+++ src/main/java/com/spring/implementation/controller/PatientController.java
@@ -0,0 +1,49 @@
+package com.spring.implementation.controller;
+
+import com.spring.implementation.dto.PatientDTO;
+import com.spring.implementation.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+
+
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientDTO>> get() {
+        return ResponseEntity.ok(patientService.getAllPatient());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PatientDTO> getPatientById(@PathVariable Long id) {
+        return ResponseEntity.ok(patientService.getPatientById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<PatientDTO> addNewPatient(@RequestBody PatientDTO patientDTO) {
+        return ResponseEntity.ok(patientService.addNewPatient(patientDTO));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<PatientDTO> patch(@RequestBody PatientDTO patientDTO, @PathVariable Long id) {
+        return ResponseEntity.ok(patientService.updatePatientName(id,patientDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> changePatient(@PathVariable Long id,@RequestBody PatientDTO patientDTO) {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
+    }
+}
